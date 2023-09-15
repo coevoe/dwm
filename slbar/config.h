ac28cd3d316b994a59dd58d5d823c0fe28f7ec89
coevoe <grastlinka@gmail.com>
2023-09-15 09:23:44 +0200

    Initial commit

---
+++ slbar/config.h
@@ -0,0 +1,26 @@
+/* See LICENSE file for copyright and license details. */
+
+#define SCRIPTDIR "/home/oizero/.local/bin/"
+/* separator used for separating modules */
+static const char *sep = " | ";
+/* length of the given separator */
+static const uint sep_len = 3;
+
+/* If update interval is set to 0 then given command will be executed only
+ * after receiving given signal. If update signal is set to 0, then there is no
+ * signal that will trigger execution of the command.
+ * To send signal use command 'kill -n $(pidof slbar)'
+ * where n is 34 + update signal. */
+static const Module modules[] = {
+  /* command                  update interval    update signal */
+  { SCRIPTDIR "dwm-cpu.sh",         2,                 0 },
+  { SCRIPTDIR "dwm-temp.sh",        2,                 0 },
+  { SCRIPTDIR "dwm-mem.sh",         2,                 0 },
+  { SCRIPTDIR "dwm-disk.sh",     3600,                 0 },
+  { SCRIPTDIR "dwm-krnl.sh",     7200,                 0 },
+  { SCRIPTDIR "dwm-bat.sh",        20,                 0 },
+  { SCRIPTDIR "dwm-back.sh",        0,                11 },
+  { SCRIPTDIR "dwm-audio.sh",       0,                10 },
+  { SCRIPTDIR "dwm-xkb.sh",         0,                12 },
+  { SCRIPTDIR "dwm-date.sh",        1,                 0 },
+};
